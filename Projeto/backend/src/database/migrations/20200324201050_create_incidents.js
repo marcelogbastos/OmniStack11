c82fdc192492194acd4215561023f73a68d0956e
Marcelo G. Bastos <marcelogbastos@gmail.com>
2020-03-25 09:28:36 -0300

    Arrumando o Banco de Dados

--- Projeto/backend/src/database/migrations/20200324201050_create_incidents.js
+++ Projeto/backend/src/database/migrations/20200324201050_create_incidents.js
@@ -1,15 +1,15 @@
 
 exports.up = function(knex) {
-    knex.schema.createTable('incidents', function (table) {
+    return knex.schema.createTable('incidents', function (table) {
         table.increments();
 
         table.string('title').notNullable();
         table.string('description').notNullable();
-        table.string('value').notNullable();
+        table.decimal('value').notNullable();
         
         table.string('ong_id').notNullable();
 
-        table.foreing('ong_id').references('id').inTable('ongs');
+        table.foreign('ong_id').references('id').inTable('ongs');
       });
 };
 
